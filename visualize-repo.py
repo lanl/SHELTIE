6a5bf52767a1306da2ccb30c9500012ad18380da
jkitson <jkitson@lanl.gov>
2018-07-23 09:17:07 -0600

    moved graph and log-dict factory functions into cleansing.py so that they can be used outside of visualize-repo.py

--- visualize-repo.py
+++ visualize-repo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-from cleansing import parse_sublogs
+from cleansing import parse_sublogs, build_graph, build_log_dict
 
 import git
 import networkx
@@ -8,56 +8,12 @@ import sys
 import os.path
 import matplotlib.pyplot as plt
 from matplotlib import colors
+import numpy as np
 import matplotlib
 import subprocess
 import pprint
 
 PRODUCTIVITY_NOTES_NAMESPACE = 'refs/notes/productivity'
-
-def build_graph(repo):
-	graph = networkx.DiGraph()
-
-	def iter_parents(commit):
-		#don't process commits twice
-		if commit not in graph.nodes:
-			
-			graph.add_node(commit)
-		
-		for parent in commit.parents:
-			#print(parent)
-			
-			#we shouldn't revist edges
-			if (commit, parent) not in graph.edges:
-				graph.add_edge(commit, parent)
-				iter_parents(parent)
-
-	for branch_head in repo.branches:
-		iter_parents(branch_head.commit)
-	return graph
-
-def build_log_dict(repo_graph, repo_dir):
-	logs_dict = {}
-	for commit in repo_graph.nodes:
-		#based on Stephen's work in analysis.py
-		try:
-			log = subprocess.check_output( \
-				["git", \
-				 "--git-dir",\
-				 os.path.join(repo_dir, ".git"),\
-				 "notes", \
-				 "--ref", \
-				 PRODUCTIVITY_NOTES_NAMESPACE, \
-				 "show", \
-				 commit.hexsha])
-			#print(log)
-			
-			#only hold onto the first sublog for now
-			logs_dict[commit] = parse_sublogs(log)[0]
-		except Exception as e:
-			#print(e)
-			pass
-	
-	return logs_dict
 
 def filter_out_commits_without_logs(graph, logs):
 	nodes = list(graph.nodes())
@@ -71,6 +27,26 @@ def filter_out_commits_without_logs(graph, logs):
 						if (pred, desc) not in graph.edges:
 							graph.add_edge(pred, desc)
 			graph.remove_node(commit)
+
+	return graph
+
+def filter_out_commits_not_between_logged_commits(graph, logs):
+	nodes = list(graph.nodes())
+	for commit in nodes:
+		if commit not in logs:
+			after_log=False
+			before_log =False
+			for pred in graph.predecessors(commit):
+				if pred in logs:
+					after_log=True
+					break
+			if after_log:
+				for succ in graph.successors(commit):
+					if succ in logs:
+						before_log=True
+						break
+			if not after_log and not before_log:
+				graph.remove_node(commit)
 
 	return graph
 
@@ -172,29 +148,58 @@ def colour_by_branch(graph, branches, logs, cmap=plt.get_cmap('jet')):
 		#print(branch, 1.0 * branches[branch] / len(branches))
 	return colours
 
-def colour_by_log_val(graph, branches, logs, get_log_val, cmap=plt.get_cmap('seismic')):
-	named_colours = colors.get_named_colors_mapping()
+def colour_by_log_val(graph, branches, logs, get_log_val, cmap='seismic', norm=colors.Normalize()):
+	grey = colors.get_named_colors_mapping()['tab:gray']
+	grey = colors.to_rgba(grey)
+	cmap = plt.get_cmap(cmap)
 	colours = []
 	for commit in graph.nodes:
-		log = logs[commit]
 		try:
+			log = logs[commit]
 			#should be normalised between 0 and 1
 			log_val = get_log_val(log) 
-			print(log_val)
-			colours.append(cmap(log_val))
+			colour = cmap(log_val)
+			colours.append(colour)
+			#print(log_val, colour)
 		except Exception as e:
-			print('log_val not found')
-			print(e)
-			colours.append(named_colours['tab:gray'])
+			#print('log_val not found')
+			#print(e)
+			colours.append(grey)
 
 	return colours
 
-def wrap_colour_by_log(get_log_val):
+def wrap_colour_by_log(get_log_val, cmap='seismic', norm=colors.Normalize()):
 	return lambda graph, branches, logs: \
-		colour_by_log_val(graph, branches, logs, get_log_val)
+		colour_by_log_val(graph, branches, logs, get_log_val, cmap)
 
 colour_by_frustration = \
-	wrap_colour_by_log(lambda log: 1.0 * log['log']['user_responses']['NASA-TLX']['frustration'] / 7.0)
+	wrap_colour_by_log(lambda log: \
+		1.0 * log['log']['user_responses']['NASA-TLX']['frustration'] / 7.0)
+#colour_by_kokkos = \
+#	wrap_colour_by_log(lambda log: \
+#		float(log['log']['user_responses']['tags']['Kokkos']), \
+#	                   cmap='binary', \
+#	                   norm=colors.BoundaryNorm(boundaries=[-0.2,-0.1,0.1,0.9,1.1,1.2]), \
+#	                                            n_colors=256)
+
+#from Stephen's analysis code
+def get_delta_sloc(commit, repo_dir=sys.argv[1]):
+	lines_changed = 0
+	#print commit.diff(commit.parents[0])[0].diff
+	diff = subprocess.check_output(["git", "--git-dir", os.path.join(repo_dir, ".git"),  'diff', commit.hexsha, commit.parents[0].hexsha])
+	sloc = 0
+	for line in diff.split('\n'):
+			if not (line.startswith("-") or line.startswith("+")): continue
+			if line.strip() == "+" or line.strip() == "-": continue
+			if line.startswith('+++') or line.startswith('---'): continue
+			sloc += 1
+	return sloc	
+
+def colour_by_delta_sloc(graph, branches, logs, cmap='coolwarm'):
+	cmap = plt.get_cmap(cmap)
+	colours = [get_delta_sloc(commit) for commit in graph.nodes]
+
+	return colours
 
 #build everything we need for ploting
 repo_dir = sys.argv[1]
@@ -202,9 +207,11 @@ repo = git.Repo(repo_dir)
 graph = build_graph(repo)
 logs = build_log_dict(graph, repo_dir)
 print('--- logs parsed ---')
+#print(graph)
 
-graph = filter_out_commits_without_logs(graph, logs)
-
+#graph = filter_out_commits_without_logs(graph, logs)
+graph = filter_out_commits_not_between_logged_commits(graph, logs)
+posns, branches = dist_with_time_order_by_branch_layout(graph, repo, logs)
 #posns, branches = dist_from_head_by_branch_layout(graph, repo, logs)
 #posns, branches = time_by_branch_layout(graph, repo, logs)
 
@@ -214,17 +221,25 @@ graph = filter_out_commits_without_logs(graph, logs)
           # time_by_branch_layout,
           # dist_with_time_order_by_branch_layout]
 layouts = [time_by_branch_layout]
-colourings = [colour_by_frustration,
-              colour_by_branch]
+layouts = [#networkx.shell_layout,
+           networkx.spring_layout
+           #networkx.spectral_layout
+          ]
+colourings = [colour_by_frustration
+              #colour_by_branch,
+              #colour_by_kokkos,
+              #colour_by_delta_sloc
+             ]
 jet = plt.get_cmap('jet')
 fig = plt.figure(1)
 
 for i in range(len(layouts)):
 	for j in range(len(colourings)):
-		posns, branches = layouts[i](graph, repo, logs)
+		#posns, branches = layouts[i](graph, repo, logs)
 		colours = colourings[j](graph, branches, logs)
 		#colours = colour_by_branch(graph, branches)
 		#posns = networkx.kamada_kawai_layout(graph)
+		posns = layouts[i](graph)
 		#print(posns)
 
 		subplot = fig.add_subplot(1, \
@@ -236,23 +251,26 @@ for i in range(len(layouts)):
 			b_list[branches[branch]] = branch
 
 		#for branch in b_list:
+		#	label = str(branch)
+		#	if label == 'sharrell':
+		#		label = 'Working Branch'
 		#	subplot.plot([0], [0],
 		#						marker='o', \
 		#						color=jet(1.0 * branches[branch] / len(branches)),\
-		#						label=str(branch))
+		#						label=label)
 		#	#print(branch, 1.0 * branches[branch] / len(branches))
 		##plot it (colouring with reference to [1])
-		#plt.subplot(subplot)
+		plt.subplot(subplot)
 		
 		#pprint.pprint(posns)
-		pprint.pprint(colours)
+		#pprint.pprint(colours)
 		networkx.draw_networkx(graph, \
 										posns, \
 										#cmap=jet, \
 										node_color=colours, \
 										node_size=50, \
 										edge_width=1, \
-										arrows=False, \
+										arrows=True, \
 										with_labels=False, \
 										ax=subplot)	
 		plt.axis('off')
